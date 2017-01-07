3a258aa6a0ad057dfca7a623de03749e4898ba7a
derpdesp <adamopouloudespoina@gmail.com>
2017-01-08 00:03:22 +0200

    it has begun

---
+++ overview.php
@@ -0,0 +1,108 @@
+<html>
+<head>
+	<title>
+		Euromed2016 | Overview
+	</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+  <div id="page">
+    <div id="logo">
+  		<a href="index.php">
+  			<img src="images/logo2.png"/>
+  		</a>
+  	</div>
+    <div id="search">
+  		<form method="get">
+    		<input type="text" name="search" placeholder="Search...">
+    		<input type="image" src="images/search_icon.png">
+  		</form>
+  	</div>
+  	<div id="profile">
+  		<form method="get">
+    		<input type="image" src="images/profile_icon.png" width="28" height="28">
+    		<a href="#">Sign in</a> | <a href="#">Sign up</a>
+  		</form>
+  	</div>
+  	<div id="menu">
+  	  <ul>
+  		  <li class="dropdown">
+      		<a href="#p" class="dropbtn_active">PROGRAM</a>
+    	    <div class="dropdown-content">
+        		<a href="overview.php">Overview</a>
+          	<a href="keynotes.php">Keynote Speakers</a>
+          	<a href="#">Workshops</a>
+          	<a href="#">Timetable</a>
+        	</div>
+      	</li>
+      	<li class="dropdown">
+      		<a href="#" class="dropbtn">VENUE</a>
+    	    <div class="dropdown-content">
+        		<a href="#">Venue Details</a>
+          	<a href="#">Travel and Transport </a>
+          	<a href="#">About Cyprus</a>
+        	</div>
+      	</li>
+      	<li class="dropdown">
+      		<a href="#" class="dropbtn">CALL FOR PAPERS</a>
+    	    <div class="dropdown-content">
+        		<a href="#">Paper Submission</a>
+        		<a href="#">Submission Guidelines</a>
+          </div>
+      	</li>
+      	<li class="dropdown">
+      		<a href="#" class="dropbtn">EXHIBITION</a>
+    	    <div class="dropdown-content">
+    		    <a href="#">Useful Info</a>
+        		<a href="#">Exhibitors</a>
+        		<a href="#">Register as Exhibitor</a>  		
+          </div>
+      	</li>
+    		<li><a href="register.php">REGISTER</a></li>
+    		<li><a href="sponsors.php">SPONSORS</a></li>
+    	</ul>
+    </div>
+    <div id="dates">
+    <table class="sidetable">
+      <tr>
+        <th><a href="#">Important Dates</a></th>
+      </tr>
+      <tr>
+        <td>Date 1</td>
+      </tr>
+      <tr>
+        <td>Date 2</td>
+      </tr>
+      <tr>
+        <td>Date 3</td>
+      </tr>
+    </table>
+    </div>
+    <div id="announs">
+    <table class="sidetable">
+      <tr>
+        <th><a href="#">Announcements</a></th>
+      </tr>
+      <tr>
+        <td>Announcement 1</td>
+      </tr>
+      <tr>
+        <td>Announcement 2</td>
+      </tr>
+      <tr>
+        <td>Announcement 3</td>
+      </tr>
+    </table>
+    </div>
+    <div id="overview">
+      <h2>Overview of the Conference</h2>
+      <p>
+        The International Conference on Cultural Heritage (CH) brings together researchers, policy makers, professionals and practitioners to explore some of the more pressing issues concerning cultural heritage today. In particular, the main goal of the conference is to focus on interdisciplinary and multi-disciplinary research on tangible and intangible Cultural Heritage, the use of cutting edge technologies for the protection, restoration, preservation, massive digitalization, documentation and presentation of the CH content. At the same time, the event is intended to cover topics of research ready for exploitation, demonstrating the acceptability of new sustainable approaches and new technologies by the user community, SME’s, owners, managers and conservators of cultural patrimony. Several organizations have decided to join together in order to create an optimal environment for the discussion, explanation of new technologies, exchange of modern ideas and in general to allow the transfer of knowledge between a maximum number of professionals and participants during one common time period.
+      </p>
+    </div>
+
+    <div id="contact">
+      <a href="#">Contact us</a> | <a href="#">Facebook</a> | <a href="#">Twitter</a>
+    </div>
+  </div>
+</body>
